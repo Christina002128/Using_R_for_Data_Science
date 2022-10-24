957143e23f1c147cb67852e9c9eb309a1c8004e9
张笑妍 <zhangxiaoyan@zhangxiaoyandeMacBook-Pro.local>
2022-10-24 15:15:51 +0100

    first commit

---
+++ W6_R_git.R
@@ -0,0 +1,8 @@
+*\
+git init
+git add *
+  git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/stomlinson-teaching/test.git git push -u origin main
+(modified from github.com)
+*\
